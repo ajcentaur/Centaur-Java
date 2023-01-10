cd1f2ef7d1b3305f158a773276efde8b327ac5fd
CentaurLuo <luochengbo@sciecomm.cn>
2023-01-10 15:56:40 +0800

    leetcode:34

--- src/main/java/com/ajcentaur/leetcode/arraygroup/BinarySearch.java
+++ src/main/java/com/ajcentaur/leetcode/arraygroup/BinarySearch.java
@@ -4,7 +4,8 @@ public class BinarySearch {
     public static void main(String[] args) {
 //        System.out.println(search704(new int[]{5},5));
 //        System.out.println(search704two(new int[]{-1,0,3,5,9,12},13));
-        System.out.println(searchInsert(new int[]{1,3,5,6},4));
+//        System.out.println(searchInsert(new int[]{1,3,5,6},4));
+        System.out.println(searchRange(new int[]{1,2,2,3,3,3,6},3));
     }
 
     /**
@@ -73,6 +74,43 @@ public class BinarySearch {
             }
         }
         return left;
+    }
+
+    /**
+     * leetcode:34
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] searchRange(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int leftIndex = -1;
+        int rightIndex = -1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] > target){
+                right = mid - 1;
+            }else if(nums[mid] < target){
+                left = mid + 1;
+            }else{
+                leftIndex = mid;
+                rightIndex = mid;
+                break;
+            }
+        }
+        if(left > right){
+            return new int[]{leftIndex, rightIndex};
+        }
+        //查找左边界
+        while(leftIndex > 0 && nums[leftIndex] == nums[leftIndex - 1]){
+            leftIndex--;
+        }
+        //查找右边界
+        while(rightIndex < nums.length - 1 && nums[rightIndex] == nums[rightIndex + 1]){
+            rightIndex++;
+        }
+        return new int[]{leftIndex, rightIndex};
     }
 
 }
