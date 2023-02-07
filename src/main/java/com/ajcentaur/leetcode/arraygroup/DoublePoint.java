387d25725c5864da4484d9963e565cb282a1fa4e
CentaurLuo <luochengbo@sciecomm.cn>
2023-02-07 16:22:12 +0800

    leet

---
+++ src/main/java/com/ajcentaur/leetcode/arraygroup/DoublePoint.java
@@ -0,0 +1,24 @@
+package com.ajcentaur.leetcode.arraygroup;
+
+public class DoublePoint {
+    public static void main(String[] args) {
+        System.out.println(removeElement(new int[]{0,1,2,2,3,0,4,2},2));
+    }
+
+    /**
+     * leetcode:27
+     * @param nums
+     * @param val
+     * @return
+     */
+    private static int removeElement(int[] nums, int val){
+        int slow = 0;
+        for(int fast = 0; fast < nums.length; fast++){
+            if(nums[fast] != val){
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
+}
