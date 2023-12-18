ae30e5b3b5bec23bdd75171c912946554cf77d86
CentaurLuo <luochengbo@sciecomm.cn>
2023-12-18 20:02:16 +0800

    commit

--- src/main/java/com/ajcentaur/juc/pool/ExecutorDemo.java
+++ src/main/java/com/ajcentaur/juc/pool/ExecutorDemo.java
@@ -1,13 +1,16 @@
 package com.ajcentaur.juc.pool;
 
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedTransferQueue;
 
 public class ExecutorDemo {
     public static void main(String[] args) {
-//        Executors.newScheduledThreadPool(5);
+        Executors.newScheduledThreadPool(5);
+        Executors.newCachedThreadPool();
 //        Thread.currentThread().interrupt();
 //        Runnable runnable = () -> System.out.println("执行成功");
 //        new Thread(runnable).start();
+        LinkedTransferQueue linkedTransferQueue = new LinkedTransferQueue();
         System.out.println("JVM虚拟机可用的处理器数量：" + Runtime.getRuntime().availableProcessors());
     }
 }
