7d2e105001bad1095d7688cb032496b0d0429961
CentaurLuo <luochengbo@sciecomm.cn>
2023-12-21 13:25:06 +0800

    commit

--- src/main/java/com/ajcentaur/juc/thread/Test.java
+++ src/main/java/com/ajcentaur/juc/thread/Test.java
@@ -3,10 +3,24 @@ package com.ajcentaur.juc.thread;
 public class Test {
 
     public static void main(String[] args) {
-        int i = 0;
-        int j = 9;
-        int k = i + j;
-        System.out.println(k);
+//        int i = 0;
+//        int j = 9;
+//        int k = i + j;
+//        System.out.println(k);
+        forDemo();
+    }
+
+    private static void forDemo(){
+        //自旋
+        for (int i = 0; ;i++) {
+            System.out.println("自旋" + i);
+            if(i > 1000){
+                System.out.println("返回");
+                // 结束方法
+                return;
+            }
+        }
+//        System.out.println("结束自旋");
     }
 
 }
